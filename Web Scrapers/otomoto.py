27360bc90fa5cfa778cde2db18a69ed61da14435
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-11-11 17:01:18 +0100

    oto_moto_mini_scraper

--- Web Scrapers/otomoto.py
+++ Web Scrapers/otomoto.py
@@ -42,8 +42,9 @@ for page in range(0,100):
 
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd 
 
-scraped_cars = []
+scraped_cars = pd.DataFrame()
 
 current_url = "https://www.otomoto.pl/osobowe/?page="  
 
@@ -56,10 +57,36 @@ def only_main_page(url):
 
 
 def main_page_details(url):
-    r = requests.get(base_url)
+    
+    url = "https://www.otomoto.pl/osobowe/?page=1"  
+
+    r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     all_cars = soup.find_all('article')
+    
+    car_df = pd.DataFrame()
+    car_list = []
 
+    main_header = soup.select('.offer-item__content > .offer-item__title > .offer-title > a')
+    car_title = ([link['title'].strip() for link in main_header])
+    car_link = ([link['href'].strip() for link in main_header])
+
+    price_header = soup.select('.offer-item__content > .offer-item__price > .offer-price > .offer-price__number')
+    price = ([link.get_text().strip() for link in price_header])
+
+
+
+
+    #create new pandas df
+    s_car_title = pd.Series(car_title)
+    s_car_link = pd.Series(car_link)
+    s_price = pd.Series(price)
+    
+    
+    car_df["car_title"] = s_car_title.values
+    car_df["car_link"] = s_car_link.values
+    car_df["car_price"] = s_price.values
+    
     
 only_main_page(current_url)
 
