e7e0c6f56432bb5573d8571eea3e5bdef710d852
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-11-11 17:27:58 +0100

    Otomoto _ version 0.1

--- Web Scrapers/otomoto.py
+++ Web Scrapers/otomoto.py
@@ -4,49 +4,13 @@ Created on Sun Nov 11 12:57:45 2018
 
 @author: piotr
 """
-
-import requests
-from bs4 import BeautifulSoup
-
-
-l = []
-for page in range(0,100):
-    page = page+1
-    base_url = "https://www.otomoto.pl/osobowe/?page="+str(page)        
-
-    r = requests.get(base_url)
-    soup = BeautifulSoup(r.text, "html.parser")
-    all_cars = soup.find_all('article')
-  
-    d = { }
-    
-    link_section = soup.select('.offer-item__content > div > h2 > .offer-title__link')
-    car_link = ([link['href'].strip() for link in link_section])
-    car_title = ([link['title'].strip() for link in link_section])
-    d['car_link'] = car_link
-    d['car_title'] = car_title
-    l.append(d)
-    
-    link_section = soup.select('.offer-item__content > .offer-item__price > .offer-price > .offer-price__number')
-    price = ([link.get_text().strip() for link in link_section])
-    d['price'] = price
-    l.append(d)
-    
-    link_section = soup.select('.offer-item__content > .offer-item__params')
-    years = ([link.find_all(attrs={"data-code": "year"}) for link in link_section])
-    d['years'] = years
-    l.append(d)
-
-
-###
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd 
 
 scraped_cars = pd.DataFrame()
 
-current_url = "https://www.otomoto.pl/osobowe/?page="  
+
 
 def only_main_page(url):
     for page in range (0,2):
@@ -73,6 +37,10 @@ def main_page_details(url):
 
     price_header = soup.select('.offer-item__content > .offer-item__price > .offer-price > .offer-price__number')
     price = ([link.get_text().strip() for link in price_header])
+    
+    year_header = soup.select('.offer-item__content > .offer-item__params')
+    years = ([link.find_all(attrs={"data-code": "year"}) for link in year_header])
+
 
 
 
@@ -81,13 +49,17 @@ def main_page_details(url):
     s_car_title = pd.Series(car_title)
     s_car_link = pd.Series(car_link)
     s_price = pd.Series(price)
-    
+    s_years = pd.Series(years)
     
     car_df["car_title"] = s_car_title.values
     car_df["car_link"] = s_car_link.values
     car_df["car_price"] = s_price.values
-    
-    
+    car_df["years"] = s_years.values
+
+    scraped_cars = scraped_cars.append(car_df)
+
+current_url = "https://www.otomoto.pl/osobowe/?page="  
+
 only_main_page(current_url)
 
               
