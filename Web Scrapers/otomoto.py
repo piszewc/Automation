0407767ef1095909fac97dd362ff0721722ebf71
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2018-11-11 18:36:54 +0100

    otomoto_mini final

--- Web Scrapers/otomoto.py
+++ Web Scrapers/otomoto.py
@@ -8,22 +8,19 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd 
 
-scraped_cars = pd.DataFrame()
-
 
 
 def only_main_page(url):
-    for page in range (0,2):
+    for page in range (0,1000):
         page+=1
         current_url = url+str(page)  
         print("Currenlty Working on page #: "+str(page))
         main_page_details(current_url)
 
-
 def main_page_details(url):
     
-    url = "https://www.otomoto.pl/osobowe/?page=1"  
-
+    scraped_cars = pd.DataFrame()
+    
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     all_cars = soup.find_all('article')
@@ -38,29 +35,32 @@ def main_page_details(url):
     price_header = soup.select('.offer-item__content > .offer-item__price > .offer-price > .offer-price__number')
     price = ([link.get_text().strip() for link in price_header])
     
-    year_header = soup.select('.offer-item__content > .offer-item__params')
-    years = ([link.find_all(attrs={"data-code": "year"}) for link in year_header])
-
-
-
-
-
+    year_header = soup.find_all(attrs={"data-code": "year"})
+    year_list = []
+    for i in year_header:
+        i = str(i)
+        year_list.append(str(i[60:64]))
+    
     #create new pandas df
     s_car_title = pd.Series(car_title)
     s_car_link = pd.Series(car_link)
     s_price = pd.Series(price)
-    s_years = pd.Series(years)
+    s_years = pd.Series(year_list)
     
     car_df["car_title"] = s_car_title.values
     car_df["car_link"] = s_car_link.values
     car_df["car_price"] = s_price.values
-    car_df["years"] = s_years.values
+    
+    car_df['car_price'], car_df['currency'] = car_df['car_price'].str.split('  ', 1).str
+    car_df['car_price'] = car_df['car_price'].str.strip()
+    car_df['currency'] = car_df['currency'].str.strip()
 
-    scraped_cars = scraped_cars.append(car_df)
+    car_df["years"] = s_years.values
+    #car_df['years'] = car_df['years'].map(lambda x: x.lstrip('+-').rstrip('aAbBcC'))
+   
+    car_df.to_csv("car_database.csv", mode='a', header=True, encoding='utf-8', index=False)
+
 
 current_url = "https://www.otomoto.pl/osobowe/?page="  
 
 only_main_page(current_url)
-
-              
-      
