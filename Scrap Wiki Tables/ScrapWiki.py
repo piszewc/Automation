b110d87ce5dd223b1278ef436f9dfbcd824b65da
Piotr Szewc <39332276+piszewc@users.noreply.github.com>
2019-11-30 22:44:07 +0100

    Wiki tables

--- Scrap Wiki Tables/ScrapWiki.py
+++ Scrap Wiki Tables/ScrapWiki.py
@@ -12,9 +12,9 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-page = requests.get("https://en.wikipedia.org/wiki/List_of_universities_in_the_United_Kingdom_by_enrolment").text
+page_html = "https://en.wikipedia.org/wiki/List_of_best-selling_singles"
 
-
+page = requests.get(page_html).text
 soup = BeautifulSoup(page, "lxml")
 data = []
 
@@ -24,14 +24,15 @@ table_body = table.find('tbody')
 header = table_body.find_all('th')
 header = [header.text.strip() for header in header]
 
+count_tables = soup.find_all('table')
+
 rows = table_body.find_all('tr')
 for row in rows:
     cols = row.find_all('td')
     cols = [ele.text.strip() for ele in cols]
     data.append([ele for ele in cols if ele]) # Get rid of empty values
 
-data[0]=header
-
+data[0] = header
 dftable = pd.DataFrame(data[1:], columns=data[0])
 
-
+site = pd.read_html(page_html)[0]
---
+++ Scrap Wiki Tables/WikiScraper.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 21:23:56 2019
+
+@author: goto
+"""
+
